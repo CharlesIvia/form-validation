afd695cf0b1eea526393cec216ea6cb34359f6fb
Charles Ivia <iviacharles@gmail.com>
2020-06-27 11:37:38 +0300

    Created fn to show error message

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const form = document.getElementById("form"),
   email = document.getElementById("email"),
   password = document.getElementById("password"),
   passwordTwo = document.getElementById("password2");
+
+//Show input error message
+
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = "message";
+}
+
+
