bdba11fb4f3be15c357efd83f04ecd3fd6eaa4f7
Charles Ivia <iviacharles@gmail.com>
2020-06-27 11:30:25 +0300

    Fetched dom elements and set variables

---
+++ script.js
@@ -0,0 +1,6 @@
+//Fetch dom elements and set variables
+const form = document.getElementById("form"),
+  username = document.getElementById("username"),
+  email = document.getElementById("email"),
+  password = document.getElementById("password"),
+  passwordTwo = document.getElementById("password2");
